dc325dd0ba771d11e962e5b324eceb48729cb604
Dev Kha <devkhanna@outlook.com>
2020-10-09 17:59:55 -0400

    replace modal w/jumbo, style w/css

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+{/* <script> */}
+
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
+
+  //call q1a and q1b
+  // if q1a chosen, "correct"/if q1b "wrong"  
+
+// var qb1 = true;
+
+//   if(qb1 === true){
+//     $("#flash").text("correct")
+//   }esle  ($("#flash").text("Wrong!"));
+
+$(document).ready(function() {
+ 
+
+
+$("q1a").on("click", function() {
+  $("#flash").append("Correct!");
+  // alert("whoops");
+});
+
+});
+// </script>
